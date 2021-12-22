f56969d30c4c5945b4e29f2495a0ca5d1a82d1f7
Melancholy22 <56004966+Melancholy22@users.noreply.github.com>
2021-12-21 17:18:44 -0800

    Tile Collision
    -Added the layers of "Actor" and "Blocking" for walls and other NPCs
    -Changed control for player so it interacts

--- TopDownSHooter/Assets/Scripts/PlayerMovement.cs
+++ TopDownSHooter/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Field Variables
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta; //Difference between current position and where you want to be.
+    private RaycastHit2D hit; //Checks if we are allowed to go there or not
 
     // Start is called before the first frame update
     private void Start()
@@ -13,7 +15,6 @@ public class PlayerMovement : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         //Where the player wants to be
@@ -29,7 +30,18 @@ public class PlayerMovement : MonoBehaviour
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 0);
 
-        // Make the sprite actually move
-        transform.Translate(moveDelta * Time.deltaTime);
+        //Make sure we can move in the direction, by casting a box there first. If box returns null, we can move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make the sprite actually move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make the sprite actually move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
 }
