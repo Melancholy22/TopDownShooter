4b099838daa45c6105c89efc867b470badd2f58a
Melancholy22 <56004966+Melancholy22@users.noreply.github.com>
2022-01-05 15:55:34 -0800

    hehe

---
+++ TopDownSHooter/Assets/Scripts/BrownCapSword.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownCapSword : BoxCollider2D
+{
+    // Start is called before the first frame update
+    public int damagePoint = 1;
+    public float pushForce = 2.0f;
+
+    public int weaponLevel = 0;
+    private SpriteRenderer spriteR;
+
+
+    protected void Start()
+    {
+        box
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
